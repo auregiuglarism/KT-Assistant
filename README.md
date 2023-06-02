98738524bb7df32ff15d83aca61f0c83c2d86ad6
Antoine <antoinedorard@gmail.com>
2023-06-02 10:36:01 +0200

    website working + updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # KT-Assistant
+
+### Website
+A website has been implemented in order to play around with the model in an intuitive way, offering a straightforward interface for uploading text and asking questions that the model will evaluate. To get started using the website, follow the steps outlined below:
+
+1. **Install Dependencies**: Ensure you have Python installed on your machine. Then, navigate to the root directory where the `requirements.txt` file is saved and install the necessary Python packages by running the following command in your terminal:
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+   This will install all the necessary libraries and dependencies needed for the server side of the web application.
+
+2. **Run the Server**: Next, you need to start the server that will interact with the model. This can be done by running the Python script set up for this purpose. In the terminal, navigate to the directory containing the server script and run:
+
+    ```
+    python server.py
+    ```
+
+   If everything is set up correctly, you should see output indicating that the server is running, usually with a line like `* Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)`.
+
+3. **Open the Website**: Now, with the server running, you can interact with the model via the website. Open the HTML file `index.html` in any web browser by double clicking the HTML file or dragging it into an open browser window.
+
+4. **Usage**: On the website, you'll first see a textbox where you can paste a document's text or upload a text file. Once the document is uploaded or pasted, you can insert your claim in the textbox below and click 'Submit'. The model will then evaluate the claim based on the provided document, and the prediction result will be displayed on the webpage.
+
+Remember, the server must be kept running while you are using the website. If you close the server, the website will not be able to interact with the model.
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from tensorflow import keras
+from keras.models import load_model
+import nltk
 
 app = Flask(__name__)
 CORS(app)
@@ -7,23 +10,69 @@ CORS(app)
 
 class Model:
     def __init__(self):
+        print("where")
+        self.preprocessed_passage = None
+        print("are")
+        nltk.download("punkt")
+        print("you")
         self.trained = False
+        print("stuck")
+        self.model = load_model('saved_model_1')
+        print("mate")
+
+    def clean(self, text, stem_words=True):
+        import re  # for regular expressions
+        from string import punctuation
+
+        text = re.sub("\'s", " ", text)
+        text = re.sub(" whats ", " what is ", text, flags=re.IGNORECASE)
+        text = re.sub("\'ve", " have ", text)
+
+        text = re.sub("can't", "cannot", text, flags=re.IGNORECASE)
+        text = re.sub("don't", "do not", text, flags=re.IGNORECASE)
+        text = re.sub("won't", "will not", text, flags=re.IGNORECASE)
+        text = re.sub("shouldn't", "should not", text, flags=re.IGNORECASE)
+        text = re.sub("couldn't", "could not", text, flags=re.IGNORECASE)
+        text = re.sub("isn't", "is not", text, flags=re.IGNORECASE)
+        text = re.sub("wasn't", "was not", text, flags=re.IGNORECASE)
+        text = re.sub("weren't", "were not", text, flags=re.IGNORECASE)
+        text = re.sub("haven't", "have not", text, flags=re.IGNORECASE)
+        text = re.sub("hasn't", "has not", text, flags=re.IGNORECASE)
+        text = re.sub("hadn't", "had not", text, flags=re.IGNORECASE)
+
+        # remove comma between numbers, i.e. 15,000 -> 15000
+        text = re.sub('(?<=[0-9])\,(?=[0-9])', "", text)
+
+        # remove punctuation
+        text = re.sub(r'[^\w\s]', ' ', text)
+
+        # Return a list of words
+        return text
+
+    def preprocess(self, text):
+        # Apply pre-processing to data
+        t = self.clean(text)
+        #t = nltk.tokenize.word_tokenize(t)
+
+        return t
 
     def train(self, text):
         print('Training model with text: {}'.format(text))
         #
+        self.preprocessed_passage = self.preprocess(text)
         self.trained = True
 
     def predict(self, claim):
         if not self.trained:
-            return False
+            return None
         else:
-
             print('Making prediction for claim: {}'.format(claim))
-            return 'True' in claim
+            preprocessed_claim = self.preprocess(claim)
 
+            x = self.preprocessed_passage + " __SEP__ " + preprocessed_claim
+            prediction = self.model.predict([x])
 
-model = Model()
+            return prediction[0][0]
 
 
 @app.route('/path-to-model-training', methods=['POST'])
@@ -46,9 +95,14 @@ def make_prediction():
         return jsonify({'message': 'No claim provided'}), 400
 
     prediction = model.predict(data['claim'])
+    print(prediction)
+    print(type(prediction))
 
-    return jsonify({'answer': prediction})
+    return jsonify({'answer': float(prediction)})
 
 
 if __name__ == '__main__':
+    model = Model()
     app.run(host='0.0.0.0', port=5000)
+    print('Model loaded')
+    print('waiting for requests...')
